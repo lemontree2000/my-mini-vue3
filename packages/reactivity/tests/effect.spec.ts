9f147adeb4078054944978dba39df04137c9f238
lemontree2000 <gonelove.guo@gmail.com>
2022-05-03 00:36:24 +0800

    test: add effect stop adn  onstop   test

--- packages/reactivity/tests/effect.spec.ts
+++ packages/reactivity/tests/effect.spec.ts
@@ -1,4 +1,4 @@
-import { effect } from "../effect";
+import { effect, stop } from "../effect";
 import { reactivity } from "../reactivity";
 
 describe('effect', () => {
@@ -36,5 +36,69 @@ describe('effect', () => {
         expect(foo).toBe(12)
 
         expect(r).toBe("foo")
+    })
+
+    it('scheduler', () => {
+        let dummy;
+        let run: any;
+
+        const scheduler = jest.fn(() => {
+            run = runner;
+        })
+
+        const obj = reactivity({
+            foo: 1,
+        })
+
+        const runner = effect(() => {
+            dummy = obj.foo;
+        }, { scheduler })
+
+        expect(scheduler).not.toHaveBeenCalled();
+        expect(dummy).toBe(1)
+
+        obj.foo++
+        expect(scheduler).toHaveBeenCalledTimes(1);
+        expect(dummy).toBe(1);
+
+        run();
+
+        expect(dummy).toBe(2)
+
+    })
+
+    it('stop', () => {
+        let dummy;
+        const obj = reactivity({ num: 1 })
+        const runner = effect(() => {
+            dummy = obj.num
+        })
+        obj.num = 2
+        expect(dummy).toBe(2)
+
+        stop(runner)
+
+        obj.num = 3;
+        expect(dummy).toBe(2)
+
+        runner();
+        // 手动执行runner 可以更新
+        expect(dummy).toBe(3)
+    })
+
+    it('onStop', () => {
+        const obj = reactivity({ num: 1 })
+
+        const onStop = jest.fn()
+
+        let dummy;
+
+        const runner = effect(() => {
+            dummy = obj.num
+        }, { onStop })
+
+        stop(runner)
+
+        expect(onStop).toBeCalledTimes(1);
     })
 })
