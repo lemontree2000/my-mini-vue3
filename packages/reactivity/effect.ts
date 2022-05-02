56814e2cf83c76e1ac4a95eab41a2126da954538
lemontree2000 <gonelove.guo@gmail.com>
2022-05-03 00:34:47 +0800

    feat: implement efffet shop fn and onsop event

--- packages/reactivity/effect.ts
+++ packages/reactivity/effect.ts
@@ -1,16 +1,39 @@
+import { extend } from "../shared";
 
 
 
 class ReactivityEffect {
-    private _fn: any;
-
-    constructor(fn) {
+    private _fn: Function;
+    public scheduler: Function | undefined;
+    deps = [];
+    active: boolean = true;
+    onStop?: () => void;
+    constructor(fn, scheduler?) {
         this._fn = fn;
+        this.scheduler = scheduler;
     }
     run() {
         activeEffect = this
+        this.active = true;
         return this._fn()
     }
+
+    stop() {
+        // 防止重复清空
+        if (this.active) {
+            cleanupEffect(this);
+            this.onStop && this.onStop()
+            this.active = false
+        }
+    }
+
+
+}
+
+function cleanupEffect(effect) {
+    effect.deps.forEach((dep: any) => {
+        dep.delete(effect);
+    });
 }
 
 // { target: {key: [activeEffect] } }
@@ -24,24 +47,46 @@ export function track(target, key) {
         targetMap.set(target, depsMap)
     }
 
-    let deps = depsMap.get(key)
-    if (!deps) {
-        deps = new Set();
-        depsMap.set(key, deps)
+    let dep = depsMap.get(key)
+    if (!dep) {
+        dep = new Set();
+        depsMap.set(key, dep)
     }
+    if (!activeEffect) return;
+
     // 每次get都会执行track Set 可以去重所以放心add
-    deps.add(activeEffect)
+    dep.add(activeEffect)
+
+    activeEffect.deps.push(dep)
 }
 
 export function trigger(target, key) {
     const depsMap = targetMap.get(target)
     const deps = depsMap.get(key)
-    deps.forEach(reactivityEffect => reactivityEffect.run())
+    deps.forEach(reactivityEffect => {
+        if (!reactivityEffect.scheduler) {
+            reactivityEffect.run()
+        } else {
+            reactivityEffect.scheduler();
+        }
+    })
+}
+
+export function stop(runner) {
+    runner.effect.stop();
 }
 
 let activeEffect;
-export function effect(fn) {
-    const _effect = new ReactivityEffect(fn);
+export function effect(fn, options: any = {}) {
+    const _effect = new ReactivityEffect(fn, options.scheduler);
+
     _effect.run();
-    return _effect.run.bind(_effect);
+
+    const runner: any = _effect.run.bind(_effect);
+
+    extend(_effect, options);
+
+    runner.effect = _effect;
+
+    return runner;
 }
