c83b25b986a24cb14e6ca430fc417c9fa55d1262
lemontree2000 <gonelove.guo@gmail.com>
2022-05-03 16:32:22 +0800

    test: add readonly nested test spec

--- packages/reactivity/tests/readonly.spec.ts
+++ packages/reactivity/tests/readonly.spec.ts
@@ -1,16 +1,27 @@
 import { isReadonly, readonly } from "../reactivity";
 
 describe('readonly', () => {
-    it('happy path', () => {
-        const original = { num: 1, bar: { baz: 2 } }
+    it('should make nested values readonly', () => {
+        const original = {
+            num: 1,
+            bar: { baz: 2 },
+            array: [
+                { bar: 2 }
+            ]
+        }
         const wrapper = readonly(original);
         expect(wrapper).not.toBe(original);
         expect(wrapper.num).toBe(1);
         expect(isReadonly(wrapper)).toBe(true)
+        
+        expect(isReadonly(wrapper.bar)).toBe(true)
+        expect(isReadonly(wrapper.array)).toBe(true)
+        expect(isReadonly(wrapper.array[0])).toBe(true)
+
         expect(isReadonly(original)).toBe(false)
     })
 
-    it('warn  when call set', () => {
+    it('should warn call when  set', () => {
         console.warn = jest.fn();
 
         const wrapper = readonly({ num: 1 })
