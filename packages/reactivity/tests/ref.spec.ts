bbc86f6ab84063f2004df460134f7ecd619cac54
lemontree2000 <gonelove.guo@gmail.com>
2022-05-03 23:08:02 +0800

    test: add proxyRefs test spec

--- packages/reactivity/tests/ref.spec.ts
+++ packages/reactivity/tests/ref.spec.ts
@@ -1,5 +1,6 @@
 import { effect } from '../effect';
-import { ref } from '../ref';
+import { reactivity } from '../reactivity';
+import { isRef, proxyRefs, ref, unRef } from '../ref';
 
 describe('ref', () => {
     it('happy path', () => {
@@ -45,6 +46,43 @@ describe('ref', () => {
         a.value.count = 2;
 
         expect(dummy).toBe(2);
+    })
+
+    it('isRef', () => {
+        const a = ref(1);
+        const user = reactivity({
+            age: 1,
+        })
+
+        expect(isRef(a)).toBe(true);
+        expect(isRef(1)).toBe(false);
+        expect(isRef(user)).toBe(false);
+    })
+
+    it('unRef', () => {
+        const a = ref(1);
+
+        expect(unRef(a)).toBe(1);
+        expect(unRef(1)).toBe(1);
+    })
+
+    it('proxyRefs', () => {
+        const user = {
+            age: ref(10),
+            name: "xiaohong"
+        }
+
+        const proxyUser = proxyRefs(user);
+
+        expect(user.age.value).toBe(10)
+        expect(proxyUser.age).toBe(10)
+        proxyUser.age = 20;
+        expect(proxyUser.age).toBe(20)
+        expect(user.age.value).toBe(20)
+
+        proxyUser.age = ref(30)
+        expect(proxyUser.age).toBe(30)
+        expect(user.age.value).toBe(30)
     })
 
 })
