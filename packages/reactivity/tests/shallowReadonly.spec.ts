5f6846826f29408247dcfa406870cbdeb354fad2
lemontree2000 <gonelove.guo@gmail.com>
2022-05-03 16:52:56 +0800

    test: add shallowReadonly test spec

---
+++ packages/reactivity/tests/shallowReadonly.spec.ts
@@ -0,0 +1,18 @@
+import { isReadonly, shallowReadonly } from "../reactivity"
+
+describe('shallowReadonly', () => {
+    it('should not make nono-reactive properties reactive', () => {
+        const props = shallowReadonly({ n: { foo: 1 } })
+
+        expect(isReadonly(props)).toBe(true)
+        expect(isReadonly(props.n)).toBe(false)
+    })
+
+    it('should warn call when  set', () => {
+        console.warn = jest.fn();
+
+        const wrapper = shallowReadonly({ num: 1 })
+        wrapper.num = 2;
+        expect(console.warn).toBeCalled()
+    })
+})
