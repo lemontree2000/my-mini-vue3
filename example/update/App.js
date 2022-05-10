e2029e98c7def0f3a68521bc0aa0805082a4021e
lemontree2000 <gonelove.guo@gmail.com>
2022-05-10 23:57:36 +0800

    feat: update example

--- example/update/App.js
+++ example/update/App.js
@@ -3,10 +3,12 @@ export const App = {
     render() {
         return h(
             "div",
-            { id: 'root', },
+            { id: 'root', ...this.props },
             [
                 h('div', {}, `Hi count:${this.count}`),
-                h('button', { onClick: this.onClick }, `click`),
+                h('button', { onClick: this.onChangePropsDemo1 }, `onChangePropsDemo1-值改变了-修改`),
+                h('button', { onClick: this.onChangePropsDemo2 }, `onChangePropsDemo1-值变成了undefined-删除`),
+                h('button', { onClick: this.onChangePropsDemo3 }, `onChangePropsDemo1-key在新的黎明没有-删除`),
             ]
         )
 
@@ -17,9 +19,30 @@ export const App = {
         const onClick = () => {
             count.value++
         }
+
+        const props = ref({
+            foo: "foo",
+            bar: "bar",
+        })
+
+        const onChangePropsDemo1 = () => {
+            props.value.foo = "new-foo"
+        }
+        const onChangePropsDemo2 = () => {
+            props.value.foo = undefined
+        }
+        const onChangePropsDemo3 = () => {
+            props.value = {
+                foo: "ffo"
+            }
+        }
         return {
             count,
+            props,
             onClick,
+            onChangePropsDemo1,
+            onChangePropsDemo2,
+            onChangePropsDemo3
         }
     }
 }
