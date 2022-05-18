4b7e98b6a0911d4d959909595279f92fed8c4a4b
lemontree2000 <gonelove.guo@gmail.com>
2022-05-18 23:56:36 +0800

    feat: add base test for parse module

---
+++ packages/compiler-core/tests/src/parse.ts
@@ -0,0 +1,15 @@
+
+export function baseParse(content: string) {
+    return {
+        children: [
+            {
+                type: "interpolation",
+                content: {
+                    type: "simple_expression",
+                    content: "message"
+                }
+            }
+        ]
+    }
+
+}
